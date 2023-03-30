ee45778239b44442bee51da802685a6af072c02f
Sulastri04 <asrilastri04@gmail.com>
2023-03-30 10:11:21 +0700

    pertemuan ke 6

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,88 +1,35 @@
-import styles from "./Movies.module.css";
-function Movies() {
-    return (
-      <div className={styles.container}>
-        <section className={styles.movies}>
-          <h2 className={styles.movies__title}>Latest Movies</h2>
-          <div className={styles.movie__container}>
-            <div className={styles.movie}>
-              <img
-                className={styles.movie__image}
-                src="https://picsum.photos/300/400"
-                alt=""
-              />
-              <h3 className={styles.movie__title}>Movie Title</h3>
-              <p className={styles.movie__date}>Date Title</p>
-            </div>
-            <div className={styles.movie}>
-              <img
-                className={styles.movie__image}
-                src="https://picsum.photos/300/400"
-                alt=""
-              />
-              <h3 className={styles.movie__title}>Movie Title</h3>
-              <p className={styles.movie__date}>Date Title</p>
-            </div>
-            <div className={styles.movie}>
-              <img
-                className={styles.movie__image}
-                src="https://picsum.photos/300/400"
-                alt=""
-              />
-              <h3 className={styles.movie__title}>Movie Title</h3>
-              <p className={styles.movie__date}>Date Title</p>
-            </div>
-            <div className={styles.movie}>
-              <img
-                className={styles.movie__image}
-                src="https://picsum.photos/300/400"
-                alt=""
-              />
-              <h3 className={styles.movie__title}>Movie Title</h3>
-              <p className={styles.movie__date}>Date Title</p>
-            </div>
-            <div className={styles.movie}>
-              <img
-                className={styles.movie__image}
-                src="https://picsum.photos/300/400"
-                alt=""
-              />
-              <h3 className={styles.movie__title}>Movie Title</h3>
-              <p className={styles.movie__date}>Date Title</p>
-            </div>
-            <div className={styles.movie}>
-              <img
-                className={styles.movie__image}
-                src="https://picsum.photos/300/400"
-                alt=""
-              />
-              <h3 className={styles.movie__title}>Movie Title</h3>
-              <p className={styles.movie__date}>Date Title</p>
-            </div>
-            <div className={styles.movie}>
-              <img
-                className={styles.movie__image}
-                src="https://picsum.photos/300/400"
-                alt=""
-              />
-              <h3 className={styles.movie__title}>Movie Title</h3>
-              <p className={styles.movie__date}>Date Title</p>
-            </div>
-            <div className={styles.movie}>
-              <img
-                className={styles.movie__image}
-                src="https://picsum.photos/300/400"
-                alt=""
-              />
-              <h3 className={styles.movie__title}>Movie Title</h3>
-              <p className={styles.movie__date}>Date Title</p>
-            </div>
-           
-          
-          </div>
-        </section>
-      </div>
-    );
+import styles from "./Movies.module.css"; 
+import Movie from "../Movie/Movie"; 
+import data from "../../utils/constants/data";
+import {useState} from "react";
+import {nanoid} from "nanoid";
+
+function Movies() { 
+  const [movies, setMovies] = useState(data);
+  function handleClick(){
+    const newFilm = {
+      id:nanoid(), 
+      title:"jigsaw",
+      year:2023, 
+      type:"Movie",
+      poster:"https://picsum.photos/300/600",
+    };
+    setMovies([...movies, newFilm]);
   }
+  return ( 
+    <div className={styles.container}> 
+      <section className={styles.movies}> 
+        <h2 className={styles.movies__title}>Latest Movies</h2> 
+        <div className={styles.movie__container}> 
+          {movies.map((movie)=>{
+            return <Movie key = {movie.id} movie={movie}/>
+          }
+          )}
+          <button onClick={handleClick}>Tambah Film</button>
+        </div> 
+      </section> 
+    </div> 
+  ); 
+} 
   
 export default Movies;
